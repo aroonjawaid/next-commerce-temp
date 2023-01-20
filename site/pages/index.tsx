30ac55eeebf93c941b1198ed308a83c578a57c62
Aroon Jawaid <aroonjawaid1@gmail.com>
2023-01-20 23:14:29 +0000

    all working files

---
+++ site/pages/index.tsx
@@ -0,0 +1,123 @@
+import commerce from '@lib/api/commerce'
+import { Layout } from '@components/common'
+import { ProductCard } from '@components/product'
+import { Grid, Marquee, Hero } from '@components/ui'
+// import HomeAllProductsGrid from '@components/common/HomeAllProductsGrid'
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export async function getStaticProps({
+  preview,
+  locale,
+  locales,
+}: GetStaticPropsContext) {
+  const config = { locale, locales }
+  const productsPromise = commerce.getAllProducts({
+    variables: { first: 6 },
+    config,
+    preview,
+    // Saleor provider only
+    ...({ featured: true } as any),
+  })
+  const pagesPromise = commerce.getAllPages({ config, preview })
+  const siteInfoPromise = commerce.getSiteInfo({ config, preview })
+  const { products } = await productsPromise
+  const { pages } = await pagesPromise
+  const { categories, brands } = await siteInfoPromise
+
+  return {
+    props: {
+      products,
+      categories,
+      brands,
+      pages,
+    },
+    revalidate: 60,
+  }
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div className="mx-auto max-w-[1380px]">
+      <div className="grid grid-cols-4 border gap-y-16">
+        {products.slice(0, 4).map((product: any, i: number) => {
+          console.log(product)
+          return (
+            <div key={product.id}>
+              <Link href={`/product/${product.slug}`}>{product.name}</Link>
+
+              <div className="border">
+                {product?.images && (
+                  <Link href={`/product/${product.slug}`}>
+                    <Image
+                      quality="100"
+                      src={product.images[0]?.url}
+                      alt={product.name || 'Product Image'}
+                      height={800}
+                      width={900}
+                      className="w-full"
+                    />
+                  </Link>
+                )}
+              </div>
+              {/* </Link> */}
+            </div>
+          )
+        })}
+      </div>
+
+      {/* <Grid variant="filled">
+        {products.slice(0, 3).map((product: any, i: number) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            imgProps={{
+              alt: product.name,
+              width: i === 0 ? 1080 : 540,
+              height: i === 0 ? 1080 : 540,
+              priority: true,
+            }}
+          />
+        ))}
+      </Grid> */}
+      {/* <Marquee variant="secondary">
+        {products.slice(0, 3).map((product: any, i: number) => (
+          <ProductCard key={product.id} product={product} variant="slim" />
+        ))}
+      </Marquee> */}
+      <div className="text-[200px]">Hello</div>
+      {/* <Hero
+        headline=" Dessert dragée halvah croissant."
+        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
+      /> */}
+      <Grid layout="B" variant="filled">
+        {products.slice(0, 3).map((product: any, i: number) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            imgProps={{
+              alt: product.name,
+              width: i === 0 ? 1080 : 540,
+              height: i === 0 ? 1080 : 540,
+            }}
+          />
+        ))}
+      </Grid>
+      <Marquee>
+        {products.slice(3).map((product: any, i: number) => (
+          <ProductCard key={product.id} product={product} variant="slim" />
+        ))}
+      </Marquee>
+      {/* <HomeAllProductsGrid
+        newestProducts={products}
+        categories={categories}
+        brands={brands}
+      /> */}
+    </div>
+  )
+}
+
+Home.Layout = Layout
